a20ba132a079dc095c268bd3d8e88e52ffc023b1
Olga Dombrovskaya <olgadombrovskaya@MacBook-Pro-2.local>
2025-08-06 10:35:49 +0200

    add Homework_04

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Button from "../Button/Button";
+import "./styles.css";
+
+function Feedback() {
+  const [likeCount, setLikeCount] = useState(0);
+  const [dislikeСount, setDislikeCount] = useState(0);
+
+  const countLike = () => {
+    setLikeCount((prevValue) => {
+      return prevValue + 1;
+    });
+  };
+  const countDislike = () => {
+    setDislikeCount((prevValue) => {
+      return prevValue + 1;
+    });
+  };
+
+  const ResetResults = () => {
+    setLikeCount((prevValue) => {
+      return (prevValue = 0);
+    });
+    setDislikeCount((prevValue) => {
+      return (prevValue = 0);
+    });
+  };
+
+  return (
+    <div className="counter_wrapper">
+      <div className="button_control">
+        <Button buttonName="Like" onClick={countLike} />
+      </div>
+      <p className="count">{likeCount}</p>
+      <div className="button_control">
+        <Button buttonName="Dislike" onClick={countDislike} />
+      </div>
+      <p className="count">{dislikeСount}</p>
+      <div className="button_control">
+        <Button buttonName="Reset Results" onClick={ResetResults} />
+      </div>
+    </div>
+  );
+}
+
+export default Feedback;
---
+++ src/homeworks/Homework_04/Homework_04.jsx
@@ -0,0 +1,13 @@
+import Feedback from "../../components/Feedback/Feedback";
+
+import "./styles.css";
+
+function Homework_04() {
+  return (
+    <div className="homework_04_page_wrapper">
+      <Feedback />
+    </div>
+  );
+}
+
+export default Homework_04;
