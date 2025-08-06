c069413fe57c7c988a249044bc42471f6905c4b8
Olga Dombrovskaya <olgadombrovskaya@MacBook-Pro-2.local>
2025-08-06 08:46:36 +0200

    chenges in files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 // Lessons
-// import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
-// import Lesson_03 from "./lessons/Lesson_03/Lesson_03/Lesson_03";
-// import Lesson_04 from "./lessons/Lesson_04/Lesson_04/Lesson_04";
+import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
+import Lesson_03 from "./lessons/Lesson_03/Lesson_03/Lesson_03";
+import Lesson_04 from "./lessons/Lesson_04/Lesson_04";
 
 // Homeworks
-// import Homework_02 from "./homeworks/Homework_02/Homework_02";
+import Homework_02 from "./homeworks/Homework_02/Homework_02";
 import Homework_03 from "./homeworks/Homework_03/Homework_03";
 
 import "./App.css";
@@ -15,11 +15,11 @@ function App() {
       {/* Lessons */}
       {/* <Lesson_02 /> */}
       {/* <Lesson_03 /> */}
-      {/* <Lesson_04 /> */}
+      <Lesson_04 />
 
       {/* Homeworks */}
       {/* <Homework_02 /> */}
-      <Homework_03 />
+      {/* <Homework_03 /> */}
 
     </div>
   );
--- src/lessons/Lesson_04/LoginForm/LoginForm.jsx
+++ src/lessons/Lesson_04/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import Button from "../Button/Button";
+import Button from "../../../components/Button/Button";
 import Input from "../Input/Input";
 
 import "./styles.css";
